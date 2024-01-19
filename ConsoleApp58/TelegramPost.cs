e4cc552c543606c7d3208063597e8362d914fb3c
sulton <global-sultonmalik326@gmail.com>
2024-01-19 14:51:33 +0500

    yangilandi

--- ConsoleApp58/TelegramPost.cs
+++ ConsoleApp58/TelegramPost.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading;
 using System.Threading.Tasks;
 using Telegram.Bot;
@@ -10,6 +11,9 @@ using Telegram.Bot.Polling;
 using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
 using Telegram.Bot.Types.ReplyMarkups;
+using Telegram.Bot.Types.InlineQueryResults;
+using System.IO.Compression;
+using System.Reflection.Metadata;
 
 namespace ConsoleApp55
 {
@@ -38,6 +42,7 @@ namespace ConsoleApp55
             botClient.StartReceiving(
                 updateHandler: HandleUpdateAsync,
                 pollingErrorHandler: HandlePollingErrorAsync,
+                
                 receiverOptions: receiverOptions,
                 cancellationToken: cts.Token
             );
@@ -54,13 +59,32 @@ namespace ConsoleApp55
 
         }
 
+
+
+
+
+
         public async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
         {
-            if (update.Message is not { } message || message.Text is not { } messageText)
-                return;
+           /* if (update.Message is not { } message || message.Text is not { } messageText)
+                return;*/
 
             var chatId = update.Message.Chat.Id;
-           /* var message = update.Message;*/
+            var message = update.Message;
+            
+
+
+
+
+
+
+
+
+
+
+            /* var message = update.Message;*/
+
+
 
 
 
@@ -86,27 +110,98 @@ namespace ConsoleApp55
         cancellationToken: cancellationToken);
                  }*/
 
-            if (message.Text == "pool")
+            /*   if (message.Text == "pool")
+               {
+                   await botClient.SendPollAsync(
+      chatId: "-1001963625643",
+      question: "Did you ever hear the tragedy of Darth Plagueis The Wise?",
+      options: new[]
+      {
+           "Yes for the hundredth time!",
+           "No, who`s that?"
+      },
+      cancellationToken: cancellationToken);
+               }else if (messageText == "/video")
+               {
+                   var fileStreamName = new FileStream(@"C:\Users\Hp\Videos\sevgi.mp4", FileMode.Open);
+
+                   await botClient.SendVideoAsync(
+       chatId: chatId,
+       video: InputFile.FromStream(fileStreamName),
+       thumbnail: InputFile.FromUri("https://raw.githubusercontent.com/TelegramBots/book/master/src/2/docs/thumb-clock.jpg"),
+       supportsStreaming: true,
+       cancellationToken: cancellationToken);
+
+
+
+               }*/
+            /* else if (messageText == "albom")
+             {
+                 // using Telegram.Bot.Types.ReplyMarkups;
+
+                 ReplyKeyboardMarkup replyKeyboardMarkup = new(new[]
+                 {
+     new KeyboardButton[] { "Help me" },
+     new KeyboardButton[] { "Call me ☎️" },
+ })
+                 {
+                     ResizeKeyboard = true
+                 };
+
+                 Message sentMessage = await botClient.SendTextMessageAsync(
+                     chatId: chatId,
+                     text: "Choose a response",
+                     replyMarkup: replyKeyboardMarkup,
+                     cancellationToken: cancellationToken);
+
+
+
+             }*//*else if(messageText =="Help me")
+             {
+                 ReplyKeyboardMarkup replyKeyboardMarkup = new(new[]
+               {
+     new KeyboardButton[] { "back" },
+     new KeyboardButton[] { "qaytti" },
+ })
+                 {
+                     ResizeKeyboard = true
+                 };
+
+                 Message sentMessage = await botClient.SendTextMessageAsync(
+                     chatId: chatId,
+                     text: "Choose a response",
+                     replyMarkup: replyKeyboardMarkup,
+                     cancellationToken: cancellationToken);
+
+             }
+             else if (messageText == "back")
+             {
+                 await BactoStart(botClient, chatId, cancellationToken);
+
+             }
+             else*/ /*if(messageText=="qaytti")
+             {
+                  await botClient.SendContactAsync(
+     chatId: chatId,
+     phoneNumber: "+1234567890",
+     firstName: "Han",
+     vCard: "BEGIN:VCARD\n" +
+            "VERSION:3.0\n" +
+            "N:Solo;Han\n" +
+            "ORG:Scruffy-looking nerf herder\n" +
+            "TEL;TYPE=voice,work,pref:+1234567890\n" +
+            "EMAIL:hansolo@mfalcon.com\n" +
+            "END:VCARD",
+     cancellationToken: cancellationToken);
+
+             }else*/
+            if (message.Type == MessageType.Document)
             {
-                await botClient.SendPollAsync(
-   chatId: "@djdbbkbwdbqbw",
-   question: "Did you ever hear the tragedy of Darth Plagueis The Wise?",
-   options: new[]
-   {
-        "Yes for the hundredth time!",
-        "No, who`s that?"
-   },
-   cancellationToken: cancellationToken);
-            }else if (messageText == "/video")
-            {
-                var fileStreamName = new FileStream(@"C:\Users\Hp\Videos\sevgi.mp4", FileMode.Open);
-     
-                await botClient.SendVideoAsync(
-    chatId: chatId,
-    video: InputFile.FromStream(fileStreamName),
-    thumbnail: InputFile.FromUri("https://raw.githubusercontent.com/TelegramBots/book/master/src/2/docs/thumb-clock.jpg"),
-    supportsStreaming: true,
-    cancellationToken: cancellationToken);
+               // await using Stream stream = System.IO.File.OpenRead(@"C:\Users\Hp\Pictures\.Net darslari/Useful_bot.zip");
+                await botClient.SendDocumentAsync(
+                   chatId: chatId,
+                   document: InputFile.FromFileId(message.Document!.FileId),
+                   caption: "The Tragedy of Hamlet,\nPrince of Denmark");
 
 
 
@@ -118,7 +213,30 @@ namespace ConsoleApp55
 
 
 
+
         }
+
+        public async Task BactoStart(ITelegramBotClient botClient , long chatId , CancellationToken cancellationToken )
+        {
+            ReplyKeyboardMarkup replyKeyboardMarkup = new(new[]
+               {
+    new KeyboardButton[] { "Help me" },
+    new KeyboardButton[] { "Call me" },
+})
+            {
+                ResizeKeyboard = true
+            };
+
+           Message sentMessage = await botClient.SendTextMessageAsync(
+                chatId: chatId,
+                text: "Choose a response",
+                replyMarkup: replyKeyboardMarkup,
+                cancellationToken: cancellationToken);
+        }
+
+
+
+
 
         private async Task HandlePollingErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
         {
@@ -131,5 +249,9 @@ namespace ConsoleApp55
             Console.WriteLine(errorMessage);
             await Task.CompletedTask;
         }
-    }
+
+
+
+      
+        }
 }
---
+++ ConsoleApp58/WTelegram/Client.cs
@@ -0,0 +1,13 @@
+﻿
+namespace WTelegram
+{
+    internal class Client
+    {
+        private Func<string, string?> getEnvironmentVariable;
+
+        public Client(Func<string, string?> getEnvironmentVariable)
+        {
+            this.getEnvironmentVariable = getEnvironmentVariable;
+        }
+    }
+}
